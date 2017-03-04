a0a27705af5b731666126039fb6820395d34ea02
longshihan <577093937@qq.com>
2017-03-04 10:35:10 +0800

    并发

---
+++ src/Zhengze.java
@@ -0,0 +1,26 @@
+/*
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+*/
+/**
+ * Created by longshihan on 2016/12/10.
+ *//*
+
+public class Zhengze {
+    public static void main(String args[]) {
+        function3();
+    }
+
+    private static void function3() {
+        String string = "ni jia hao,ming tian bu fang jia le!";
+        String regex = "\\b[a-z]{3}\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(string);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+            System.out.println(matcher.start() + "-" + matcher.end());
+        }
+    }
+}
+*/
